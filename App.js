0a443ccf2fb8d8dd2e02a9f8f82bcc72059edae6
kimberlyflrs <kimberly.marleny@gmail.com>
2019-01-16 21:55:32 -0800

    Login Screen visual

--- App.js
+++ App.js
@@ -7,34 +7,59 @@
  */
 
 import React, {Component} from 'react';
-import {Text, View, Button} from 'react-native';
+import {Text, View, Button, Image, StyleSheet} from 'react-native';
+import {SocialIcon} from 'react-native-elements';
 
+
+const styles = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  image: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
+
+const sty = StyleSheet.create({
+  image: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+  });
 
 export default class StartPage extends Component{
   _login(){
-    alert("login!"); 
+    alert("login!");
   }
 
   _signUp(){
-    alert("signup!"); 
+    alert("signup!");
   }
+
 
   render(){
     return(
-      <View>
-        <View>
-          <Text>VidPT App</Text>
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, justifyContent:'center'}}>
+          <Image source={require('./logo.png')} style={{alignSelf: 'center'}}/>
         </View>
-        <View>
-          <Button
-            onPress={this._login}
-            title="Login"
-          />
-          <Button
-            onPress={this._signUp}
-            title="Sign Up"
-          />
-        </View>
+
+        <SocialIcon
+          title='Sign In With Facebook'
+          button
+          type='facebook'
+          onPress={this._login}
+        />
+
+        <SocialIcon
+          title='Sign In With Google'
+          button
+          type='google-plus-official'
+        />
+
       </View>
     );
 
