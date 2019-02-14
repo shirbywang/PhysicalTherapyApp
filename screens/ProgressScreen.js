9496dbfcd774575557efc1e32706c999584d7ef3
kimberlyflrs <kimberly.marleny@gmail.com>
2019-02-13 19:58:52 -0800

    testing graph

--- screens/ProgressScreen.js
+++ screens/ProgressScreen.js
@@ -11,6 +11,15 @@ import {Text, View, Button, Image} from 'react-native';
 import Icon from 'react-native-vector-icons/dist/Ionicons';
 
 
+//use npm to add react-chartkick chart.js
+
+import ReactChartkick, { LineChart, PieChart } from 'react-chartkick'
+import Chart from 'chart.js'
+
+ReactChartkick.addAdapter(Chart)
+
+
+
 class ProgressScreen extends Component {
 
   static navigationOptions = {
@@ -25,10 +34,20 @@ class ProgressScreen extends Component {
     }
   }
 
+recoveryAnalysis = [
+  {"name": "blah", "data": {"2019-02-01": 1, "2019-02-02": 2}},
+  {"name": "klh", "data": {"2019-02-01": 3, "2019-02-02": 4}}
+];
+
+//        <LineChart data={{"2017-05-13": 2, "2017-05-14": 5}} />
+
+
+
+
   render() {
     return (
       <View style={{flex: 1, backgroundColor: '#111d53'}}>
-        <Text style={{color:'white', fontWeight: 'bold', fontSize: 20, paddingTop: 25, paddingLeft: 10, paddingBottom: 25}}>Progress </Text>
+        <Text style={{color:'white', fontWeight: 'bold', fontSize: 20, paddingTop: 25, paddingLeft: 10, paddingBottom: 25}}>Progress</Text>
 
         <Image source={require('../progressplaceholder.png')} style={{alignSelf: 'center', height: 175, width:350}}/>
 
